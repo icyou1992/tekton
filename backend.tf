267480e058d0ead2231415f143f5ca6ddafdf863
icyou1992 <icyou1992@gmail.com>
2023-09-07 14:00:03 +0900

    change backend

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "tfstate-pfe-056231226580"
+    key            = "tfstate/pfe/tekton.tfstate"
+    region         = "ap-northeast-2"
+    dynamodb_table = "tfstate-pfe-056231226580"
+    encrypt        = true
+  }
+}
